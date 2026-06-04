8714a9f51097f55185f5f3457f1efb578f861de9
Joshua Mellott-Lillie <10jmellott@gmail.com>
2026-06-04 18:58:30 -0400

    Updated PWA manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,10 +17,29 @@ export default defineNuxtConfig({
     }
   },
   pwa: {
-		registerType: 'autoUpdate',
-		appManifest: true,
-		devOptions: {
-			enabled: true
-		}
-	} as PwaModuleOptions
+    registerType: 'autoUpdate',
+    appManifest: true,
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: "Zen Manga",
+      short_name: "Zen Manga",
+      icons: [
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ],
+      theme_color: "#e07a5f",
+      background_color: "#3d405b",
+      display: "standalone"
+    }
+  } as PwaModuleOptions
 });
