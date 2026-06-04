9a8f63add93cc73bc47a881f057e6bc5310c8585
Joshua Mellott-Lillie <10jmellott@gmail.com>
2026-06-04 19:28:07 -0400

    Next attempted fix for manifest icon path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,12 +27,12 @@ export default defineNuxtConfig({
       short_name: "Zen Manga",
       icons: [
         {
-          src: "/android-chrome-192x192.png",
+          src: "/zen-manga/android-chrome-192x192.png",
           sizes: "192x192",
           type: "image/png"
         },
         {
-          src: "/android-chrome-512x512.png",
+          src: "/zen-manga/android-chrome-512x512.png",
           sizes: "512x512",
           type: "image/png"
         }
